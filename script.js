e081427d5ce18788f06ec39c9e26a0b7e311f72d
Anil Kumar <114513976+ak8058@users.noreply.github.com>
2025-02-02 11:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function makeid(l) {
   // write your code here
+	let res="";
+	let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(let i=0;i<l;i++)
+		{
+			let ranindex=Math.floor(Math.random()*str.length);
+			res=res+str[ranindex];
+		}
+	return res;
+	
 }
 
+
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+ const l = prompt("Enter a number.");
+ alert(makeid(l));
